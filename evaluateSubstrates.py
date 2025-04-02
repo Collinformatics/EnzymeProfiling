3771d8f2032ac1429c0d4614a8cde5b24663a891
Collin Andrews <pizzaTrucks@outlook.com>
2025-04-02 15:50:22 -0500

    Update evaluateSubstrates.py

--- evaluateSubstrates.py
+++ evaluateSubstrates.py
@@ -16,8 +16,8 @@ from functions import filePaths, NGS
 
 # ===================================== User Inputs ======================================
 # Input 1: Select Dataset
-inEnzymeName = 'Fyn'
-inBasePath = f'/Users/ca34522/Documents/Research/NGS/{inEnzymeName}'
+inEnzymeName = 'name'
+inBasePath = f'/path/to/folder/{inEnzymeName}'
 inFilePath = os.path.join(inBasePath, 'Extracted Data')
 inSavePathFigures = os.path.join(inBasePath, 'Figures')
 inFileNamesInitial, inFileNamesFinal, inAAPositions = filePaths(enzyme=inEnzymeName)
