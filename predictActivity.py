1d8af040afbf42b35fe3b66f3743f9a6b1a942cb
Collin Andrews <pizzaTrucks@outlook.com>
2025-06-07 11:08:36 -0500

    Add files via upload

--- predictActivity.py
+++ predictActivity.py
@@ -1,3 +1,5 @@
+import os.path
+
 import esm
 import numpy as np
 import pandas as pd
@@ -10,6 +12,7 @@ from xgboost import XGBRegressor, XGBRFRegressor, DMatrix
 
 
 inAAPositions = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8']
+enzymeName = f'SARS-CoV-2 M{'ᵖʳᵒ'}'
 substrates = {
     'VVLQSAFE': 357412,
     'VALQAASA': 311191,
@@ -67,58 +70,6 @@ class CNN:
 
 
 
-class RandomForestRegressor:
-    def __init__(self, dfTrain, dfTest, getSHAP=False):
-        print('=========================== Random Forrest Regressor '
-              '============================')
-        print(f'Module: {purple}XGBoost{resetColor}')
-        subsPred = list(dfTest.index)
-
-        # Record the embeddings for the predicted substrates
-        self.dTest = DMatrix(dfTest)
-
-        # Process dataframe
-        x = dfTrain.drop(columns='activity').values
-        y = np.log1p(dfTrain['activity'].values)
-
-
-        # Train the model
-        start = time.time()
-        self.model = XGBRFRegressor(device=device, tree_method="hist")
-        self.model.fit(x, y)
-        end = time.time()
-        runtime = (end - start) * 1000
-        print(f'Training time: {red}{round(runtime, 3):,} ms{resetColor}\n')
-
-        # Save model
-        
-
-        if getSHAP:
-            # Get: SHAP values
-            booster = self.model.get_booster()
-            booster.set_param({'device': device})
-            shapValues = booster.predict(self.dTest, pred_contribs=True)
-            shapInteractionValues = booster.predict(self.dTest, pred_interactions=True)
-            print(f'Shap Values:\n{shapValues}\n\n'
-                  f'Interaction Values:\n{shapInteractionValues}\n\n')
-
-
-        # Predict with the model
-        print(f'Predicting Activity')
-        self.model = 'cpu'
-        start = time.time()
-        activityPred = self.model.predict(dfTest)
-        activityPred = np.expm1(activityPred)  # Reverse log1p transform
-        for index, substrate in enumerate(subsPred):
-            print(f'     {substrate}: {red}{activityPred[index]}{resetColor}')
-        end = time.time()
-        runtime = (end - start) * 1000
-        # print(f'Predicted Activity:\n'
-        #       f'{activityPred}')
-        print(f'Testing time: {red}{round(runtime, 3):,} ms{resetColor}\n')
-
-
-
 class GradBoostingRegressor:
     def __init__(self, dfTrain, dfTest):
         print('========================== Gradient Boosting Regressor '
@@ -133,12 +84,13 @@ class GradBoostingRegressor:
         y = np.log1p(dfTrain['activity'].values)
 
         # Train the model
+        print(f'Training the model')
         start = time.time()
         self.model = GradientBoostingRegressor()
         self.model.fit(x, y)
         end = time.time()
         runtime = (end - start) * 1000
-        print(f'Training time: {red}{round(runtime, 3):,} ms{resetColor}\n')
+        print(f'      Training time: {red}{round(runtime, 3):,} ms{resetColor}\n')
 
         self.predActivity()
 
@@ -161,12 +113,13 @@ class GradBoostingRegressorXGB:
         y = np.log1p(dfTrain['activity'].values)
 
         # Train the model
+        print(f'Training the model')
         start = time.time()
         self.model = XGBRegressor(device=device)
         self.model.fit(x, y)
         end = time.time()
         runtime = (end - start) * 1000
-        print(f'Training time: {red}{round(runtime, 3):,} ms{resetColor}\n')
+        print(f'     Training time: {red}{round(runtime, 3):,} ms{resetColor}\n')
 
         self.predActivity()
 
@@ -175,117 +128,228 @@ class GradBoostingRegressorXGB:
 
 
 
+class RandomForestRegressor:
+    def __init__(self, dfTrain, dfTest, modelName, getSHAP=False):
+        print('=========================== Random Forrest Regressor '
+              '============================')
+        print(f'Module: {purple}XGBoost{resetColor}')
+        subsPred = list(dfTest.index)
 
-def ESM(substrates, subLabel, datasetType):
-    print('=========================== Convert To Numerical: ESM '
-          '===========================')
-    print(f'Dataset: {purple}{datasetType}{resetColor}\n'
-          f'Total unique substrates: {red}{len(substrates):,}{resetColor}')
+        # Record the embeddings for the predicted substrates
+        self.dTest = DMatrix(dfTest)
 
-    # Step 1: Convert substrates to ESM model format and generate embeddings
-    totalSubActivity = 0
-    subs = []
-    values = []
-    if type(substrates) is dict:
-        for index, (substrate, value) in enumerate(substrates.items()):
-            totalSubActivity += value
-            subs.append((f'Sub{index}', substrate))
-            values.append(value)
-    else:
-        for index, substrate in enumerate(substrates):
-            subs.append((f'Sub{index}', substrate))
-    sampleSize = len(substrates)
-    print(f'Collected substrates:{red} {sampleSize:,}{resetColor}')
-    if totalSubActivity != 0:
-        if isinstance(totalSubActivity, float):
-              print(f'Total Values:{red} {round(totalSubActivity, 1):,}'
-                    f'{resetColor}')
+        # Process dataframe
+        x = dfTrain.drop(columns='activity').values
+        y = np.log1p(dfTrain['activity'].values)
+
+
+        # Save model
+        pathModel = os.path.join('models', f'{modelName}.ubj') # ubj: Binary JSON file
+        if os.path.exists(pathModel):
+            print(f'Loading Trained ESM Model:\n'
+                  f'     {greenDark}{pathModel}{resetColor}\n\n')
+
+            # Load the model
+            self.model = XGBRFRegressor()
+            self.model.load_model(pathModel)
         else:
-            print(f'Total Values:{red} {totalSubActivity:,}{resetColor}')
-    print()
+            # Train the model
+            print(f'Training the model')
+            start = time.time()
+            model = XGBRFRegressor(device=device, tree_method="hist")
+            model.fit(x, y)
+            end = time.time()
+            runtime = (end - start) * 1000
+            print(f'     Training time: {red}{round(runtime, 3):,} ms{resetColor}\n')
+
+            print(f'Saving Trained ESM Model:\n'
+                  f'     {greenDark}{pathModel}{resetColor}\n\n')
+            model.save_model(pathModel)
+
+        if getSHAP:
+            # Get: SHAP values
+            booster = self.model.get_booster()
+            booster.set_param({'device': device})
+            shapValues = booster.predict(self.dTest, pred_contribs=True)
+            shapInteractionValues = booster.predict(self.dTest, pred_interactions=True)
+            print(f'Shap Values:\n{shapValues}\n\n'
+                  f'Interaction Values:\n{shapInteractionValues}\n\n')
 
 
+        # Predict with the model
+        print(f'Predicting Activity')
+        start = time.time()
+        activityPred = self.model.predict(dfTest)
+        activityPred = np.expm1(activityPred)  # Reverse log1p transform
+        end = time.time()
+        runtime = (end - start) * 1000
+        print(f'     Runtime: {red}{round(runtime, 3):,} ms{resetColor}\n')
 
-    # Step 2: Load the ESM model and batch converter
-    model, alphabet = esm.pretrained.esm2_t36_3B_UR50D()
-    numLayersESM = 36
-    # model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-    # numLayersESM = 33
-    # esm2_t36_3B_UR50D has 36 layers
-    # esm2_t33_650M_UR50D has 33 layers
-    # esm2_t12_35M_UR50D has 12 layers
-    model = model.to(device)
-
-    # Get batch tensor
-    batchConverter = alphabet.get_batch_converter()
-
-
-    # Step 3: Convert substrates to ESM model format and generate embeddings
-    try:
-        batchLabels, batchSubs, batchTokens = batchConverter(subs)
-        batchTokensCPU = batchTokens
-        batchTokens = batchTokens.to(device)
-
-    except Exception as exc:
-        print(f'{orange}ERROR: The ESM has failed to evaluate your substrates\n\n'
-              f'Exception:\n{exc}\n\n'
-              f'Suggestion:'
-              f'     Try replacing: {cyan}esm.pretrained.esm2_t36_3B_UR50D()'
-              f'{orange}\n'
-              f'     With: {cyan}esm.pretrained.esm2_t33_650M_UR50D()'
-              f'{resetColor}\n')
-        sys.exit(1)
-    print(f'Batch Tokens:{greenLight} {batchTokens.shape}{resetColor}\n'
-          f'{greenLight}{batchTokens}{resetColor}\n')
-
-    # Record tokens
-    slicedTokens = pd.DataFrame(batchTokensCPU[:, 1:-1],
-                                index=batchSubs,
-                                columns=subLabel)
-    if totalSubActivity != 0:
-        slicedTokens['Values'] = values
-    print(f'\nSliced Tokens:\n'
-          f'{greenLight}{slicedTokens}{resetColor}\n')
-
-    with torch.no_grad():
-        results = model(batchTokens, repr_layers=[numLayersESM], return_contacts=False)
-
-    # Step 4: Extract per-sequence embeddings
-    tokenReps = results["representations"][numLayersESM]  # (N, seq_len, hidden_dim)
-    sequenceEmbeddings = tokenReps[:, 0, :]  # [CLS] token embedding: (N, hidden_dim)
-    print(f'Extracted embeddings shape: '
-          f'{pink}{sequenceEmbeddings.shape}{resetColor}\n\n')
-
-    # Convert to numpy and store substrate activity proxy
-    embeddings = sequenceEmbeddings.cpu().numpy()
-    if type(substrates) is dict:
-        values = np.array(values).reshape(-1, 1)
-        data = np.hstack([embeddings, values])
-        columns = [f'feat_{i}' for i in range(embeddings.shape[1])] + ['activity']
-    else:
-        data = np.hstack([embeddings])
-        columns = [f'feat_{i}' for i in range(embeddings.shape[1])]
-    subEmbeddings = pd.DataFrame(data, index=batchSubs, columns=columns)
-
-    return subEmbeddings
+        # Evaluate prediction
+        predictions = {}
+        for index, substrate in enumerate(subsPred):
+            predictions[substrate] = activityPred[index]
+        predictions = dict(sorted(predictions.items(), key=lambda x: x[1], reverse=True))
+        print('Predicted Activity:')
+        for iteration, (substrate, value) in enumerate(predictions.items()):
+            value = float(value)
+            print(f'     {substrate}: {red}{round(value, 3):,}{resetColor}')
+            if iteration >= 10:
+                break
+        print('\n')
 
 
 
 class PredictActivity:
-    def __init__(self, subsTrain, subsTest):
+    def __init__(self, enzymeName, datasetTag, subsTrain, subsTest, labelsXAxis):
+        self.enzymeName = enzymeName
+        self.datasetTag = datasetTag
+        self.labelsXAxis = labelsXAxis
+        self.subsTrain = subsTrain
+        self.subsTest = subsTest
+        self.modelNameESM = ''
+
+        # Make sure the directory exists
+        os.makedirs('embeddings', exist_ok=True)
+        os.makedirs('models', exist_ok=True)
+
         # Generate embeddings
-        embedingsSubsTrain = ESM(substrates=subsTrain, subLabel=inAAPositions,
-                        datasetType='Template')
-        embedingsSubsPred = ESM(substrates=subsTest, subLabel=inAAPositions,
+        embedingsSubsTrain = self.ESM(substrates=self.subsTrain, subLabel=inAAPositions,
+                        datasetType=self.datasetTag, trainModel=True)
+        embedingsSubsPred = self.ESM(substrates=self.subsTest, subLabel=inAAPositions,
                        datasetType='Predictions')
         
         # cnn = CNN(substrates=substrates)
-        RandomForestRegressor(dfTrain=embedingsSubsTrain, dfTest=embedingsSubsPred)
+        RandomForestRegressor(dfTrain=embedingsSubsTrain, dfTest=embedingsSubsPred,
+                              modelName=self.modelNameESM)
         sys.exit()
         GradBoostingRegressor(dfTrain=embedingsSubsTrain, dfTest=embedingsSubsPred)
         GradBoostingRegressorXGB(dfTrain=embedingsSubsTrain, dfTest=embedingsSubsPred)
 
 
 
+    def ESM(self, substrates, subLabel, datasetType, trainModel=False):
+        print('=========================== Convert To Numerical: ESM '
+              '===========================')
+
+        # Choose: ESM model
+        modelPrams = 1
+        if modelPrams == 0:
+            sizeESM = '15B Params'
+        else:
+            sizeESM = '3B Params'
+        tagEmbeddings = f'{self.enzymeName} - ESM {sizeESM} - {datasetType}'
+        if trainModel:
+            self.modelNameESM = tagEmbeddings
+        print(f'Dataset: {purple}{tagEmbeddings}{resetColor}\n'
+              f'Total unique substrates: {red}{len(substrates):,}{resetColor}\n')
+
+        # Load: ESM embeddings
+        pathEmbeddings = os.path.join('embeddings', f'{tagEmbeddings}.csv')
+        if os.path.exists(pathEmbeddings):
+            print(f'Loading: ESM Embeddings\n'
+                  f'     {greenDark}{pathEmbeddings}{resetColor}\n')
+            subEmbeddings = pd.read_csv(pathEmbeddings, index_col=0)
+            print(f'Substrate embeddings shape: '
+                  f'{pink}{subEmbeddings.shape}{resetColor}\n\n')
+
+            return subEmbeddings
+
+
+        # Step 1: Convert substrates to ESM model format and generate embeddings
+        totalSubActivity = 0
+        subs = []
+        values = []
+        if type(substrates) is dict:
+            for index, (substrate, value) in enumerate(substrates.items()):
+                totalSubActivity += value
+                subs.append((f'Sub{index}', substrate))
+                values.append(value)
+        else:
+            for index, substrate in enumerate(substrates):
+                subs.append((f'Sub{index}', substrate))
+        sampleSize = len(substrates)
+        print(f'Collected substrates:{red} {sampleSize:,}{resetColor}')
+        if totalSubActivity != 0:
+            if isinstance(totalSubActivity, float):
+                print(f'Total Values:{red} {round(totalSubActivity, 1):,}'
+                      f'{resetColor}')
+            else:
+                print(f'Total Values:{red} {totalSubActivity:,}{resetColor}')
+        print()
+
+        # Step 2: Load the ESM model and batch converter
+        if sizeESM == '15B Params':
+            model, alphabet = esm.pretrained.esm2_t48_15B_UR50D()
+            numLayersESM = 48
+        else:
+            model, alphabet = esm.pretrained.esm2_t36_3B_UR50D()
+            numLayersESM = 36
+        # esm2_t36_3B_UR50D has 36 layers
+        # esm2_t33_650M_UR50D has 33 layers
+        # esm2_t12_35M_UR50D has 12 layers
+        model = model.to(device)
+
+        # Get batch tensor
+        batchConverter = alphabet.get_batch_converter()
+
+        # Step 3: Convert substrates to ESM model format and generate embeddings
+        try:
+            batchLabels, batchSubs, batchTokens = batchConverter(subs)
+            batchTokensCPU = batchTokens
+            batchTokens = batchTokens.to(device)
+
+        except Exception as exc:
+            print(f'{orange}ERROR: The ESM has failed to evaluate your substrates\n\n'
+                  f'Exception:\n{exc}\n\n'
+                  f'Suggestion:'
+                  f'     Try replacing: {cyan}esm.pretrained.esm2_t36_3B_UR50D()'
+                  f'{orange}\n'
+                  f'     With: {cyan}esm.pretrained.esm2_t33_650M_UR50D()'
+                  f'{resetColor}\n')
+            sys.exit(1)
+        print(f'Batch Tokens:{greenLight} {batchTokens.shape}{resetColor}\n'
+              f'{greenLight}{batchTokens}{resetColor}\n')
+
+        # Record tokens
+        slicedTokens = pd.DataFrame(batchTokensCPU[:, 1:-1],
+                                    index=batchSubs,
+                                    columns=subLabel)
+        if totalSubActivity != 0:
+            slicedTokens['Values'] = values
+        print(f'\nSliced Tokens:\n'
+              f'{greenLight}{slicedTokens}{resetColor}\n')
+
+        with torch.no_grad():
+            results = model(batchTokens, repr_layers=[numLayersESM],
+                            return_contacts=False)
+
+        # Step 4: Extract per-sequence embeddings
+        tokenReps = results["representations"][numLayersESM]  # (N, seq_len, hidden_dim)
+        sequenceEmbeddings = tokenReps[:, 0, :]  # [CLS] token embedding: (N, hidden_dim)
+
+        # Convert to numpy and store substrate activity proxy
+        embeddings = sequenceEmbeddings.cpu().numpy()
+        if type(substrates) is dict:
+            values = np.array(values).reshape(-1, 1)
+            data = np.hstack([embeddings, values])
+            columns = [f'feat_{i}' for i in range(embeddings.shape[1])] + ['activity']
+        else:
+            data = np.hstack([embeddings])
+            columns = [f'feat_{i}' for i in range(embeddings.shape[1])]
+
+        # Process embeddings
+        subEmbeddings = pd.DataFrame(data, index=batchSubs, columns=columns)
+        print(f'Substrate embeddings shape: '
+              f'{pink}{sequenceEmbeddings.shape}{resetColor}\n\n')
+        print(f'Embeddings saved at:\n'
+              f'     {greenDark}{pathEmbeddings}{resetColor}\n\n')
+        subEmbeddings.to_csv(pathEmbeddings)
+
+        return subEmbeddings
+
+
+
 # =================================== Initialize Class ===================================
-PredictActivity(subsTrain=substrates, subsTest=substratesPred)
+PredictActivity(enzymeName=enzymeName, datasetTag='Template', labelsXAxis=inAAPositions,
+                subsTrain=substrates, subsTest=substratesPred)
