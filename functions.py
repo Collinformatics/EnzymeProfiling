b4227ae50c50ff0d12f5b6857810b1b75e0184da
Collin Andrews <pizzaTrucks@outlook.com>
2025-06-09 22:57:44 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -5499,7 +5499,7 @@ class RandomForestRegressorCombo:
         self.device = device
         self.paramGrid = {
             'n_estimators': [100, 200, 300],
-            'max_depth': [10, 20, 30],
+            'max_depth': [5, 10, 15],
             'min_samples_split': [2, 5, 10],
             'min_samples_leaf': [1, 2, 4]
         }
