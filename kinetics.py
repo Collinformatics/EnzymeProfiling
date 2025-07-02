c741776345b178b4db8fbc91f12ee6fb514f2178
Collin Andrews <pizzaTrucks@outlook.com>
2025-07-02 15:24:05 -0500

    Add files via upload

--- kinetics.py
+++ kinetics.py
@@ -1,5 +1,4 @@
-from tokenize import group
-
+import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -13,10 +12,11 @@ inEnzymeName = 'Mpro2'
 inFileName = 'Kinetics template-SVTFQSAVK'
 inPathFolder = f'{inEnzymeName}/Kinetics'
 inSheetName = ['Product standard', 'SVTFQSAVK_Raw data']
+inSubstrate = inFileName.split('-')[1]
 
 # Input 2: Process Data
 inConcentrationUnit = 'uM'
-inPlotAllFigs = False
+inPlotAllFigs = True
 inStdCurveStartIndex = 0
 inConcCutoff = 10 # Max percentage of substrate concentration in rxn plot
 
@@ -57,7 +57,7 @@ def pressKey(event):
 def loadExcel(sheetName='', loadStandard=False):
     print('=================================== Load Data '
           '===================================')
-    print(f'Loading Excel Sheet: {pink}{sheetName}{resetColor}')
+    print(f'Loading excel sheet: {pink}{sheetName}{resetColor}')
     path = os.path.join(inPathFolder, inFileName)
     if '.xlsx' not in path:
         path += '.xlsx'
@@ -96,26 +96,28 @@ def loadExcel(sheetName='', loadStandard=False):
 
         # Group data by substrate concentration
         dataFiltered = {}
-        df = pd.DataFrame(0.0, index=[], columns=[])
         for conc, value in indexConc.items():
             # Get only the data rows, remove the first 2 rows (NaN and Time)
             filteredIndex = data.index[2:]
             triplicate = data.loc[filteredIndex, [data.columns[i] for i in value]].copy()
 
-            # 2. Redefine column headers
+            # Redefine column headers
             newHeaders = data.iloc[1, value].tolist() # row 1 = 'G1', 'H1', 'I1'
             triplicate.columns = newHeaders
+
+            # Inspect time dtype
+            if isinstance(triplicate.index[0], datetime.time):
+                triplicate.index = [
+                    (t.hour * 3600 + t.minute * 60 + t.second + t.microsecond / 1e6) / 60
+                    for t in triplicate.index
+                ]
             triplicate.index.name = indexHeader
             dataFiltered[f'{conc} {inConcentrationUnit}'] = triplicate
-
-            # Statistical analysis
-            triplicate.loc[:, 'Avg'] = round(triplicate.loc[:, :].mean(axis=1), 2)
-            triplicate.loc[:, 'StDev'] = round(triplicate.std(axis=1), 2)
 
         print(f'Loaded Data: {pink}{sheetName}{resetColor}')
         for conc, value in dataFiltered.items():
             print(f'Concentration: {purple}{conc}{resetColor}\n{value}\n\n')
-
+        # sys.exit()
 
     return dataFiltered
 
@@ -142,7 +144,8 @@ def processStandardCurve(psc, plotFig=False):
         # Scatter plot
         fig, ax = plt.subplots(figsize=(9.5, 8))
         plt.scatter(x, y, color='#BF5700', marker='o')
-        plt.title('\nProduct Standard Curve', fontsize=18, fontweight='bold')
+        plt.title(f'\nProduct Standard Curve\n{inEnzymeName}\n{inSubstrate}',
+                  fontsize=18, fontweight='bold')
         plt.xlabel(f'{psc.columns[0]}', fontsize=16)
         plt.ylabel(f'{psc.columns[1]}', fontsize=16)
         plt.grid(True)
@@ -159,46 +162,17 @@ def processStandardCurve(psc, plotFig=False):
 def processKinetics(slope, datasets, plotFig=False):
     print('=========================== Processing Kinetics Data '
           '============================')
-    print(f'Slope: {red}{round(slope, 3)}{resetColor}')
-    print(f'Slope: {red}{round(slope, 3)}{resetColor}')
-    print(datasets)
-    print('\n')
+    print(f'Slope: {red}{round(slope, 3)}{resetColor}\n')
 
-
-    # Process florescence
-    for conc, values in datasets:
-        # group = dataset[conc].copy()
-        # group.loc[:, 'Avg'] = round(group.loc[:, :].mean(axis=1), 2)
-        # group.loc[:, 'StDev'] = round(group.std(axis=1), 2)
-        group.loc[:, '[Prod]'] = round((group.loc[:, 'Avg'] / slope), 2)
-
-        print(f'Values: {purple}{conc}{resetColor}\n{group}\n\n')
-
-        # Extract product concentrations
-
-        datasets[conc] = group
-    print(f'Reactions:\n{reactions}\n\n')
-
-
-    # # Plot all reaction progress
-    # x = reactions.index
-    # fig, ax = plt.subplots(figsize=(9.5, 8))
-    #
-    # # Loop through each concentration and plot its [product] values
-    # for conc in reactions.columns:
-    #     y = reactions[conc]
-    #     ax.plot(x, y, marker='o', label=conc)  # or use ax.scatter for dots only
-    #
-    # ax.set_title('Kinetics!', fontsize=18, fontweight='bold')
-    # ax.set_xlabel('Time', fontsize=16)
-    # ax.set_ylabel('Product Concentration', fontsize=16)
-    # ax.grid(True)
-    # ax.legend(title='[Substrate]')
-    #
-    # fig.tight_layout()
-    # fig.canvas.mpl_connect('key_press_event', pressKey)
-    # plt.show()
-
+    # Process florescence data
+    print('Statistical analysis:')
+    for conc, data in datasets.items():
+        # Statistical analysis
+        data.loc[:, 'Avg'] = round(data.loc[:, :].mean(axis=1), 2)
+        data.loc[:, 'StDev'] = round(data.std(axis=1), 2)
+        data.loc[:, 'CoVar'] = data.loc[:, 'StDev'] / data.loc[:, 'Avg']
+        data.loc[:, '[Prod]'] = round((data.loc[:, 'Avg'] / slope), 2)
+        print(f'Substrate concentration: {purple}{conc}{resetColor}\n{data}\n\n')
 
 
     # Plot individual reactions
@@ -216,17 +190,18 @@ def processKinetics(slope, datasets, plotFig=False):
               f'Maximum product concentration: {red}{cutoff}{resetColor}')
 
         # Filter datapoints
-        x = values.index
-        y = values.loc[:, '[Prod]']
+        x = pd.to_numeric(values.index, errors='coerce')
+        y = pd.to_numeric(values.loc[:, '[Prod]'], errors='coerce')
         mask = y <= cutoff
         x = x[mask]
         y = y[mask]
+
 
         # Fit the datapoints
         slope, intercept = np.polyfit(x, y, 1)  # Degree 1 polynomial = linear
         fitLine = slope * x + intercept
         fit = f'y = {slope:.3f}x + {intercept:.3f}'
-        print(f'Fit: {red}{fit}{resetColor}\n')
+        print(f'Fit: {red}{fit}{resetColor}\n\n')
         reactionSlopes[conc] = slope
 
 
@@ -234,7 +209,8 @@ def processKinetics(slope, datasets, plotFig=False):
             # Scatter plot
             fig, ax = plt.subplots(figsize=(9.5, 8))
             plt.scatter(x, y, color='#BF5700', marker='o')
-            plt.title(f'Substrate Concentration: {conc}\n'
+            plt.title(f'\n{inEnzymeName}\n{inSubstrate}\n'
+                      f'Substrate Concentration: {(conc)}\n'
                       f'Maximum Product Concentration: {cutoff}',
                       fontsize=18, fontweight='bold')
             plt.xlabel(f'Time', fontsize=16)
@@ -245,7 +221,6 @@ def processKinetics(slope, datasets, plotFig=False):
             plt.legend(fontsize=12, prop={'weight': 'bold'})
             fig.canvas.mpl_connect('key_press_event', pressKey)
             plt.show()
-    print()
 
     return subConc, reactionSlopes
 
@@ -278,12 +253,14 @@ def MichaelisMenten(substrateConc, velocity):
     xFit = np.linspace(0, max(substrateConc) * 1.2, 100)
     yFit = MM(xFit, vMax, Km)
 
+    dec = 2
 
     # Plot the data
     fig, ax = plt.subplots(figsize=(9.5, 8))
     plt.scatter(substrateConc, v, color='#BF5700')
     plt.plot(xFit, yFit, color='#32D713', label='Michaelis-Menten fit')
-    plt.title(f'Michaelis-Menten\n{inEnzymeName}\nVmax = {vMax}\nKm = {Km}',
+    plt.title(f'Michaelis-Menten\n{inEnzymeName}\n{inSubstrate}\n'
+              f'Km = {round(Km, dec)}\nVmax = {round(vMax, dec)}',
               fontsize=18, fontweight='bold')
     plt.xlabel(f'[Substrate]', fontsize=16)
     plt.ylabel(f'Velocity', fontsize=16)
@@ -302,5 +279,5 @@ m = processStandardCurve(psc=prodStdCurve, plotFig=inPlotAllFigs)
 fluorescence = loadExcel(sheetName=inSheetName[1])
 
 # Process kinetics
-subConc, v = processKinetics(slope=m, dataset=fluorescence, plotFig=inPlotAllFigs)
+subConc, v = processKinetics(slope=m, datasets=fluorescence, plotFig=inPlotAllFigs)
 MichaelisMenten(substrateConc=subConc, velocity=v)
