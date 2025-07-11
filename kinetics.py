9a7671d35061f42d1489585a3a322cd0138d4303
Collin Andrews <pizzaTrucks@outlook.com>
2025-07-11 14:45:47 -0500

    Add files via upload

--- kinetics.py
+++ kinetics.py
@@ -1,23 +1,28 @@
 import datetime
+from time import time_ns
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter
+from scipy.stats import linregress
 import sys
+
 
 
 
 # Input 1: Select Dataset
 inEnzymeName = 'Mpro2'
-inFileName = 'Kinetics template-SVTFQSAVK'
+inFileName = 'Kinetics template-60nM SVILQAPFR'
 inPathFolder = f'{inEnzymeName}/Kinetics'
-inSheetName = ['Product standard', 'SVTFQSAVK_Raw data']
+inSheetName = ['Product standard', 'Sub_Raw data']
 inSubstrate = inFileName.split('-')[1]
 
 # Input 2: Process Data
+inBurstKinetics = True
 inConcentrationUnit = 'uM'
-inPlotAllFigs = False
+inPlotAllFigs = True
 inStdCurveStartIndex = 0
 inConcCutoff = 10 # Max percentage of substrate concentration in rxn plot
 inMaxCovariance = 0.3
@@ -114,11 +119,12 @@ def loadExcel(sheetName='', loadStandard=False):
                     for t in triplicate.index
                 ]
             triplicate.index.name = indexHeader
-            dataFiltered[f'{conc} {inConcentrationUnit}'] = triplicate
+            dataFiltered[conc] = triplicate
 
         print(f'Loaded Data: {pink}{sheetName}{resetColor}')
         for conc, value in dataFiltered.items():
-            print(f'Concentration: {purple}{conc}{resetColor}\n{value}\n\n')
+            print(f'Concentration: {purple}{conc} {inConcentrationUnit}{resetColor}\n'
+                  f'{value}\n\n')
         # sys.exit()
 
     return dataFiltered
@@ -179,15 +185,10 @@ def processKinetics(slope, datasets, plotFig=False):
 
     # Plot individual reactions
     reactionSlopes = {}
-    subConc = []
     for conc, values in datasets.items():
-        if 'uM' in conc:
-            numericConc = float(conc.replace('uM', ''))
-        subConc.append(numericConc)
-        cutoff = numericConc / inConcCutoff
-
-        print(f'Concentration: {red}{conc}{resetColor}\n'
-              f'Maximum product concentration: {red}{cutoff}{resetColor}')
+        cutoff = conc / inConcCutoff
+        print(f'Numeric Conc: {red}{conc} {inConcentrationUnit}{resetColor}\n'
+              f'Cutoff: {cutoff}\n\n')
 
         # Filter datapoints
         x = pd.to_numeric(values.index, errors='coerce')
@@ -198,38 +199,40 @@ def processKinetics(slope, datasets, plotFig=False):
 
 
         # Fit the datapoints
-        slope, intercept = np.polyfit(x, y, 1)  # Degree 1 polynomial = linear
-        fitLine = slope * x + intercept
-        fit = f'y = {slope:.3f}x + {intercept:.3f}'
-        print(f'Fit: {red}{fit}{resetColor}\n\n')
-        reactionSlopes[conc] = slope
+        if len(x) != 0:
+            slope, intercept = np.polyfit(x, y, 1) # Degree 1 polynomial = linear
+            fitLine = slope * x + intercept
+            fit = f'y = {slope:.3f}x + {intercept:.3f}'
+            print(f'Fit: {red}{fit}{resetColor}\n\n')
+            reactionSlopes[conc] = slope
 
 
-        if plotFig:
-            # Scatter plot
-            fig, ax = plt.subplots(figsize=(9.5, 8))
-            plt.scatter(x, y, color='#BF5700', marker='o')
-            plt.title(f'\n{inEnzymeName}\n{inSubstrate}\n'
-                      f'Substrate Concentration: {(conc)}\n'
-                      f'Maximum Product Concentration: {cutoff}',
-                      fontsize=18, fontweight='bold')
-            plt.xlabel(f'Time', fontsize=16)
-            plt.ylabel(f'Product Concentration', fontsize=16)
-            plt.grid(True)
-            plt.plot(x, fitLine, label=fit, color='#F8971F')
-            plt.tight_layout()
-            plt.legend(fontsize=12, prop={'weight': 'bold'})
-            fig.canvas.mpl_connect('key_press_event', pressKey)
-            plt.show()
+            if plotFig:
+                # Scatter plot
+                fig, ax = plt.subplots(figsize=(9.5, 8))
+                plt.scatter(x, y, color='#BF5700', marker='o')
+                plt.title(f'\n{inEnzymeName}\n{inSubstrate}\n'
+                          f'Substrate Concentration: {(conc)} {inConcentrationUnit}\n'
+                          f'Maximum Product Concentration: {cutoff}',
+                          fontsize=18, fontweight='bold')
+                plt.xlabel(f'Time', fontsize=16)
+                plt.ylabel(f'Product Concentration', fontsize=16)
+                plt.grid(True)
+                plt.plot(x, fitLine, label=fit, color='#F8971F')
+                plt.tight_layout()
+                plt.legend(fontsize=12, prop={'weight': 'bold'})
+                fig.canvas.mpl_connect('key_press_event', pressKey)
+                plt.show()
 
-    return subConc, reactionSlopes
+    return reactionSlopes
 
 
 
-def MichaelisMenten(substrateConc, velocity):
+def MichaelisMenten(velocity):
     print('=============================== Michaelis-Menten '
           '================================')
     dec = 2
+    substrateConc = list(velocity.keys())
 
 
     print('Velocities:')
@@ -249,7 +252,8 @@ def MichaelisMenten(substrateConc, velocity):
     popt, pcov = curve_fit(MM, substrateConc, v, bounds=(0, np.inf))
     vMax, Km = popt
 
-    print(f'Vmax: {red}{vMax:.3f}{resetColor}\n'
+    print(f'Dataset: {purple}{inEnzymeName} {inSubstrate}{resetColor}\n'
+          f'Vmax: {red}{vMax:.3f}{resetColor}\n'
           f'Km = {red}{Km:.3f}{resetColor}\n\n')
 
     # Fit the data
@@ -271,6 +275,114 @@ def MichaelisMenten(substrateConc, velocity):
     plt.show()
 
 
+def processKineticsBurst(slope, datasets, plotFig):
+    print('======================== Processing Burst Kinetics Data '
+          '=========================')
+    print(f'Slope: {red}{round(slope, 3)}{resetColor}\n')
+
+    # Process florescence data
+    print('Statistical analysis:')
+    for conc, data in datasets.items():
+        # Statistical analysis
+        data.loc[:, 'Avg'] = data.loc[:, :].mean(axis=1)
+        data.loc[:, 'StDev'] = data.std(axis=1)
+        data.loc[:, 'CoVar'] = data.loc[:, 'StDev'] / data.loc[:, 'Avg']
+        data.loc[:, '[Prod]'] = data.loc[:, 'Avg'] / slope
+        print(f'Substrate concentration: {purple}{conc}{resetColor}\n{data}\n\n')
+
+
+    # Plot individual reactions
+    reactionSlopes = {}
+    for conc, values in datasets.items():
+        # Get datapoints
+        x = np.array(pd.to_numeric(values.index, errors='coerce'))
+        y = np.array(pd.to_numeric(values.loc[:, '[Prod]'], errors='coerce'))
+        print(f'X, Y: {len(x)}, {len(y)}\n')
+
+        # Fit the datapoints
+        smoothedProduct = savgol_filter(y, window_length=15, polyorder=2)
+        print(f'Len(y): {len(y)}, Len(smoothedProduct): {len(smoothedProduct)}\n')
+        rate = np.gradient(smoothedProduct, x)
+        threshold = 0.5 * np.max(rate)
+        burstEndIndex = np.where(rate < threshold)[0][0]
+        print(f'Split Index: {burstEndIndex}\n')
+
+        # Evaluate splitting
+        if burstEndIndex == 0:
+            # cutoff = conc / inConcCutoff
+            # print(f'Numeric Conc: {red}{conc} {inConcentrationUnit}{resetColor}\n'
+            #       f'Cutoff: {cutoff}\n\n')
+            #
+            # # Filter datapoints
+            # mask = y <= cutoff
+            # x = x[mask]
+            # y = y[mask]
+            # print(f'X, Y: {len(x)}, {len(y)}\n')
+
+            slope, intercept = np.polyfit(x, y, 1)  # Degree 1 polynomial = linear
+            fitLine = slope * x + intercept
+            fit = f'y = {slope:.3f}x + {intercept:.3f}'
+            print(f'Fit: {red}{fit}{resetColor}\n\n')
+            reactionSlopes[conc] = slope
+
+            if plotFig:
+                # Scatter plot
+                fig, ax = plt.subplots(figsize=(9.5, 8))
+                plt.scatter(x, y, color='#BF5700', marker='o')
+                plt.title(f'\n{inEnzymeName}\n{inSubstrate}\n'
+                          f'Substrate Concentration: {conc} {inConcentrationUnit}',
+                          fontsize=18, fontweight='bold')
+                plt.xlabel(f'Time', fontsize=16)
+                plt.ylabel(f'Product Concentration', fontsize=16)
+                plt.grid(True)
+                plt.plot(x, fitLine, label=fit, color='black')
+                plt.tight_layout()
+                plt.legend(fontsize=12, prop={'weight': 'bold'})
+                fig.canvas.mpl_connect('key_press_event', pressKey)
+                plt.show()
+        else:
+            # Phase 1: Burst
+            timeBurst = x[:burstEndIndex]
+            productBurst = y[:burstEndIndex]
+
+            # Phase 2: Steady-State
+            timeSteady = x[burstEndIndex:]
+            productSteady = y[burstEndIndex:]
+            print(f'Time 1: {len(timeBurst)}, {len(productBurst)}\n'
+                  f'Time 2: {len(timeSteady)}, {len(productSteady)}\n\n')
+
+            # Fit burst phase
+            slope1, intercept1, *_ = linregress(timeBurst, productBurst)
+
+            # Fit steady-state phase
+            slope2, intercept2, *_ = linregress(timeSteady, productSteady)
+
+            reactionSlopes[conc] = slope1
+
+
+            if plotFig:
+                # Scatter plot
+                fig, ax = plt.subplots(figsize=(9.5, 8))
+                plt.scatter(x, y, color='#BF5700', marker='o')
+                plt.plot(timeBurst, slope1 * timeBurst + intercept1,
+                         label='Burst Fit', color='blue') # Burst fit
+                plt.plot(timeSteady, slope2 * timeSteady + intercept2,
+                         label='Steady-State Fit', color='red') # Steady-state fit
+                plt.title(f'\n{inEnzymeName}\n{inSubstrate}\n'
+                          f'Substrate Concentration: {conc} {inConcentrationUnit}',
+                          fontsize=18, fontweight='bold')
+                plt.xlabel(f'Time', fontsize=16)
+                plt.ylabel(f'Product Concentration', fontsize=16)
+                plt.grid(True)
+                plt.tight_layout()
+                plt.legend(fontsize=12, prop={'weight': 'bold'})
+                fig.canvas.mpl_connect('key_press_event', pressKey)
+                plt.show()
+
+    return reactionSlopes
+
+
+
 
 # ====================================== Load data =======================================
 # Load: Product Standard Curve
@@ -281,5 +393,8 @@ m = processStandardCurve(psc=prodStdCurve, plotFig=inPlotAllFigs)
 fluorescence = loadExcel(sheetName=inSheetName[1])
 
 # Process kinetics
-subConc, v = processKinetics(slope=m, datasets=fluorescence, plotFig=inPlotAllFigs)
-MichaelisMenten(substrateConc=subConc, velocity=v)
+if inBurstKinetics:
+    v = processKineticsBurst(slope=m, datasets=fluorescence, plotFig=inPlotAllFigs)
+else:
+    v = processKinetics(slope=m, datasets=fluorescence, plotFig=inPlotAllFigs)
+MichaelisMenten(velocity=v)
