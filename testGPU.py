b24f15773ec519560a4ae0896ae4f3da8da38727
Collin Andrews <pizzaTrucks@outlook.com>
2025-09-10 14:42:17 -0500

    Update testGPU.py

--- testGPU.py
+++ testGPU.py
@@ -1,5 +1,19 @@
 import torch
 
+try:
+    deviceName = torch.cuda.get_device_name(torch.cuda.current_device())
+    print(f'GPU Name: {deviceName}')
+except:
+    pass
+
+# Select device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
+
 print(f'CUDA Available: {torch.cuda.is_available()}')
 print(f'CUDA Version: {torch.version.cuda}')
-print(f'GPU Name: {torch.cuda.get_device_name(torch.cuda.current_device())}')
+print(f'Training Device: {device}\n')
